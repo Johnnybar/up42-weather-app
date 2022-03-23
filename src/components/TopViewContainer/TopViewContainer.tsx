8a2f42269365860205ec04d0db5138b964ac9949
Johnnybar <johnnybareket@gmail.com>
2022-03-23 22:34:26 +0100

    develop -  modifies TopViewContainer classes and styling

--- src/components/TopViewContainer/TopViewContainer.tsx
+++ src/components/TopViewContainer/TopViewContainer.tsx
@@ -1,35 +1,40 @@
-import React from 'react';
-import WeatherIcon from '../WeatherIcon/WeatherIcon';
-import SelectedView from '../SelectedView/SelectedView';
-import DateLocation from '../DateLocation/DateLocation';
+import React from "react";
+import WeatherIcon from "../WeatherIcon/WeatherIcon";
+import SelectedView from "../SelectedView/SelectedView";
+import DateLocation from "../DateLocation/DateLocation";
+import "./TopViewContainer.scss";
 
 interface TopViewProps {
-    selectedHour: HourWeatherProps | null;
-    dayLocationInfo: DayLocationProps | undefined;
-    hoursData: HourWeatherProps[];
-  }
+  selectedHour: HourWeatherProps | null;
+  dayLocationInfo: DayLocationProps | undefined;
+  hoursData: HourWeatherProps[];
+}
 
-const TopViewContainer = ({ selectedHour, dayLocationInfo, hoursData }: TopViewProps) => (
-     <div className="weather-app__top-view container">
-        <div className="row">
-          <div className="col-md-4">
-            {selectedHour && <WeatherIcon type={selectedHour.weather} />}
-          </div>
-          <div className="col-md-4">
-            {dayLocationInfo && selectedHour && (
-              <SelectedView
-              hiLoTemp={dayLocationInfo.hiLoTemp}
-              selectedHour={selectedHour}
-              />
-              )}
-          </div>
-          <div className="col-md-4">
-            {dayLocationInfo && hoursData && (
-              <DateLocation dayLocationInfo={dayLocationInfo} />
-              )}
-          </div>
-        </div>
-        </div>
-  );
+const TopViewContainer = ({
+  selectedHour,
+  dayLocationInfo,
+  hoursData,
+}: TopViewProps) => (
+  <div className="weather-app__top-view container-fluid">
+    <div className="row">
+      <div className="col-md-4 weather-app__weather-icon-container">
+        {selectedHour && <WeatherIcon type={selectedHour.weather} />}
+      </div>
+      <div className="col-md-4">
+        {dayLocationInfo && selectedHour && (
+          <SelectedView
+            hiLoTemp={dayLocationInfo.hiLoTemp}
+            selectedHour={selectedHour}
+          />
+        )}
+      </div>
+      <div className="col-md-4">
+        {dayLocationInfo && hoursData && (
+          <DateLocation dayLocationInfo={dayLocationInfo} />
+        )}
+      </div>
+    </div>
+  </div>
+);
 
 export default TopViewContainer;
