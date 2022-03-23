738fddb47c8f64032707f942d8d83fe817b7c057
Johnnybar <johnnybareket@gmail.com>
2022-03-23 17:11:05 +0100

    develop - adds DateLocation classes, css and layout

--- src/components/DateLocation/DateLocation.tsx
+++ src/components/DateLocation/DateLocation.tsx
@@ -1,13 +1,17 @@
+import "./DateLocation.scss";
+
 interface DateLocationProps {
   dayLocationInfo: DayLocationProps;
 }
 const DateLocation = ({ dayLocationInfo }: DateLocationProps) => {
   return (
-    <>
-      <div>{dayLocationInfo.name}</div>
+    <div className="weather-app__date-location">
+      <div className="weather-app__date-location-city">
+        {dayLocationInfo.name}
+      </div>
       <div>{dayLocationInfo.day}</div>
       <div>{dayLocationInfo.exactDate}</div>
-    </>
+    </div>
   );
 };
 export default DateLocation;
