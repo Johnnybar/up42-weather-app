ecf3f36af92fb89992b667f3273efbfbbbb0a8d2
Johnnybar <johnnybareket@gmail.com>
2022-03-23 17:10:10 +0100

    develop - adds weathericon classes, css and layout

--- src/components/WeatherIcon/WeatherIcon.tsx
+++ src/components/WeatherIcon/WeatherIcon.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import clouds from "../../assets/weather-cloud.svg";
 import clear from "../../assets/weather-sun.svg";
-
+import "./WeatherIcon.scss";
 interface WeatherIconProps {
   type: string;
 }
 const WeatherIcon = ({ type }: WeatherIconProps) => {
   return (
     <img
-      className={`${type}-weather`}
+      className="weather-app__weather-icon"
       src={type === "Clear" ? clear : clouds}
       alt={`${type} weather icon`}
     />
