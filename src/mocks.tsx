43edb1fe4a2727738246077a1ac113ada25a51e3
Johnnybar <johnnybareket@gmail.com>
2022-03-24 14:42:04 +0100

    develop - adds mocks file

---
+++ src/mocks.tsx
@@ -0,0 +1,49 @@
+export const mockHour = {
+  weather: "Clouds",
+  time: "19:00",
+  temp: "4",
+  selected: true,
+};
+
+export const mockHoursData = [
+  {
+    weather: "Clear",
+    time: "13:00",
+    temp: "7",
+    selected: false,
+  },
+  {
+    weather: "Clouds",
+    time: "15:00",
+    temp: "2",
+    selected: false,
+  },
+  {
+    weather: "Clouds",
+    time: "19:00",
+    temp: "5",
+    selected: true,
+  },
+];
+
+export const mockDayLocationInfo = {
+  name: "Berlin",
+  day: "Monday",
+  exactDate: "27.February",
+  hiLoTemp: "7/11",
+};
+
+export const mockSelectedHour = {
+  weather: "Clouds",
+  time: "19:00",
+  temp: "5",
+  selected: true,
+};
+
+export const mockSelectHour = (i: number, arr: HourWeatherProps[]) => {
+  return;
+};
+
+export const mockHiLoTemp = "7/11";
+
+export const mockError = "Error description";
