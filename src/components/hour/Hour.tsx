dac3c312e8c58994c0d88bca1fe52602abd55e86
Johnnybar <johnnybareket@gmail.com>
2022-03-24 14:43:50 +0100

    develop - implements addClassIfSelected in Hout

--- src/components/hour/Hour.tsx
+++ src/components/hour/Hour.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Hour.scss";
 import WeatherIcon from "../WeatherIcon/WeatherIcon";
+import { addClassIfSelected } from "../../utils";
 
 interface HourProps {
   index: number;
@@ -11,14 +12,28 @@ interface HourProps {
 const Hour = ({ index, hour, hoursData, selectHour }: HourProps) => {
   return (
     <li
-      className={`weather-app__hour-view ${
-        hour.selected ? "weather-app__hour-view--selected" : ""
-      }`}
+      className={`weather-app__hour-view ${addClassIfSelected(
+        hour,
+        "weather-app__hour-view--selected"
+      )}`}
       onClick={() => selectHour(index, hoursData)}
     >
-      <div className="weather-app__hour-view-time">{hour.time}</div>
+      {/* adds 'selected' class to child for testing purposes */}
+      <div
+        className={`weather-app__hour-view-time ${addClassIfSelected(
+          hour,
+          "selected"
+        )}`}
+      >
+        {hour.time}
+      </div>
       <WeatherIcon type={hour.weather} />
-      <div className="weather-app__hour-view-temp">{hour.temp}</div>
+      <div
+        className="weather-app__hour-view-temp"
+        data-testid={`${addClassIfSelected(hour, "selected-hour")}`}
+      >
+        {hour.temp}
+      </div>
     </li>
   );
 };
