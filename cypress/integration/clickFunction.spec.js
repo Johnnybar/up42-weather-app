761d6698a25cf369148f9d9a7f1eb2e93d2466fc
Johnnybar <johnnybareket@gmail.com>
2022-03-25 10:40:08 +0100

    develop - modifies cypress tests to changes class names

--- cypress/integration/clickFunction.spec.js
+++ cypress/integration/clickFunction.spec.js
@@ -2,9 +2,9 @@ describe("Click functionality", () => {
   it("should add background color to hour after clicking it", () => {
     cy.visit("/");
 
-    cy.get(".weather-app__hour-view").first().click({ force: true });
-    cy.get(`.weather-app__hour-view`)
+    cy.get(".weather-app-hour-view").first().click({ force: true });
+    cy.get(`.weather-app-hour-view`)
       .first()
-      .should("have.class", "weather-app__hour-view--selected");
+      .should("have.class", "weather-app-hour-view--selected");
   });
 });
--- cypress/integration/render.spec.js
+++ cypress/integration/render.spec.js
@@ -1,18 +1,18 @@
 describe("App", () => {
   it("should render with necessary elements", () => {
     cy.visit("/");
-    cy.get(`.weather-app__top-view`).should("be.visible");
+    cy.get(`.weather-app-top-view`).should("be.visible");
 
-    cy.get(`.weather-app__top-view`)
+    cy.get(`.weather-app-top-view`)
       .children()
       .first()
       .should("have.class", "row");
 
-    cy.get(`.weather-app__bottom-view`).should("be.visible");
+    cy.get(`.weather-app-bottom-view`).should("be.visible");
 
-    cy.get(`.weather-app__bottom-view`)
+    cy.get(`.weather-app-bottom-view`)
       .children()
       .first()
-      .should("have.class", "weather-app__bottom-view-hour-list");
+      .should("have.class", "weather-app-bottom-view__hour-list");
   });
 });
